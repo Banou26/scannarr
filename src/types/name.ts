3f1757f6dc09d06f31396059184f6b1c75b6782e
Banou <me@banou.dev>
2022-05-26 01:29:42 -0400

    feat: ✨add Name score

--- src/types/name.ts
+++ src/types/name.ts
@@ -1,7 +1,8 @@
 type Name = {
   language: string
   name: string
-  search?: boolean
+  /** From 0-1 describing the confidence of that name being used for search results */
+  score: number
 }
 
 export default Name
