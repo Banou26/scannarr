ca108c1279de32896c7bf479d793b4671824db98
Banou <me@banou.dev>
2022-07-25 02:18:46 +0200

    feat: ✨isUri util function

--- src/utils/uri.ts
+++ src/utils/uri.ts
@@ -9,6 +9,12 @@ export const fromUri = (uri: Uri) => {
 
 export const toUri = ({ scheme, id }: { scheme: string, id: string }): Uri => `${scheme}:${id}`
 
+export const isUri = (uri: string) =>
+  uri
+    .split(':')
+    .filter(part => part.length)
+    .length === 2
+
 export const populateUri = <T extends Partial<Pick<Handle, 'uri'>> & Omit<Handle, 'uri'>>(handle: T): T & Handle => ({
   ...handle,
   uri: toUri({ scheme: handle.scheme, id: handle.id })
