8f4c8926f181dcd116e17fce44bf7584fd68c3cb
Banou26 <me@banou.dev>
2022-08-10 02:09:29 +0200

    refactor: ♻️groupHandles using dependency-graph

--- src/utils/handle.ts
+++ src/utils/handle.ts
@@ -1,61 +1,41 @@
-import * as A from 'fp-ts/Array'
-import * as R from 'fp-ts/Record'
-import { pipe } from 'fp-ts/function'
-import { groupBy } from 'fp-ts/lib/NonEmptyArray'
+import { DepGraph } from 'dependency-graph'
 
 import { Handle } from '../types'
 
-// todo: replace this with a graph data structure
 export const groupHandles = <T extends Handle>(values: T[]): { groups: [string, T[]][], singles: T[] } => {
-  const handlesMap =
-    pipe(
-      values,
-      groupBy(handle => handle.uri)
-    )
-  console.log('values', values.flat())
+  const graph = new DepGraph<Handle>({ circular: true })
 
-  const groupedHandlesMap =
-    pipe(
-      handlesMap,
-      R.toEntries,
-      A.map(([uri, [handle]]) => [
-        uri,
-        [
-          handle,
-          ...values
-            .flat()
-            .filter(handle =>
-              handle.handles?.some(({ uri: innerUri }) => uri === innerUri)
-            )
-        ]
-      ] as [string, T[]]),
-      A.filter(([, arr]) => arr.length > 1),
-      R.fromEntries
-    )
+  for (const handle of values) {
+    if (!graph.hasNode(handle.uri)) graph.addNode(handle.uri, handle)
+    for (const relatedHandle of handle?.handles ?? []) {
+      if (!graph.hasNode(relatedHandle.uri)) graph.addNode(relatedHandle.uri, relatedHandle)
+      graph.addDependency(handle.uri, relatedHandle.uri)
+      graph.addDependency(relatedHandle.uri, handle.uri)
+    }
+  }
 
-  const groupedHandleSet =
-    pipe(
-      groupedHandlesMap,
-      R.toEntries,
-      A.map(([, handles]) => handles),
-      A.flatten,
-      A.map(handle => handle.uri)
-    )
-
+  const dependencies =
+    values
+      .map(handle => [
+        handle.uri,
+        ...graph.dependenciesOf(handle.uri)
+      ])
+      .map(uris =>
+        [uris[0], uris.map(uri => graph.getNodeData(uri))] as [string, T[]]
+      )
+    
   const groupedHandles =
-    pipe(
-      groupedHandlesMap,
-      R.toEntries
-    )
+    dependencies
+      .filter(handles => handles[1].length > 1)
+      .filter(([uri], i, groupedHandles) =>
+        groupedHandles
+          .findIndex(([, handles]) => handles.some(({ uri: _uri }) => _uri === uri)) !== i
+      )
 
   const singleHandles =
-    pipe(
-      values,
-      A.filter(handle => !groupedHandleSet.includes(handle.uri))
-    )
-    
-  console.log('groupedHandles', groupedHandles)
-  console.log('singleHandles', singleHandles)
+    dependencies
+      .filter(handles => handles[1].length === 1)
+      .flatMap(handles => graph.getNodeData(handles[0]) as T)
 
   return {
     groups: groupedHandles,
