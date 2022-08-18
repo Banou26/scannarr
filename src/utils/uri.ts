c134594028f37baefaa7fc919b02f7acc43f66b7
Banou <me@banou.dev>
2022-08-18 04:01:54 +0200

    feat: ✨define Uris, UriValues & fromUris

--- src/utils/uri.ts
+++ src/utils/uri.ts
@@ -1,21 +1,35 @@
 import Handle from '../types/handle'
 
 export type Uri = `${string}:${string}`
+type Separated<S extends string> = `${S}${''|`,${S}`}`
+type Uris = Separated<Separated<Uri>>
 
-export const fromUri = (uri: Uri) => {
-  const [scheme, id] = uri.split(':')
-  return { scheme: scheme!, id: id! }
+export type UriValues = {
+  id: string
+  scheme: string
 }
 
-export const toUri = ({ scheme, id }: { scheme: string, id: string }): Uri => `${scheme}:${id}`
+export const fromUri = (uri: Uri): UriValues => {
+  const [scheme, id] = uri.split(':') as [string, string]
+  return { scheme: scheme, id: id }
+}
 
-export const isUri = (uri: string) =>
+export const fromUris = <T extends string | undefined = undefined>(uriString: Uris, schemeSearch?: T): T extends string ? UriValues : UriValues[] => {
+  const uris = uriString.split(',') as Uri[]
+  const result = uris.map((uri) => fromUri(uri))
+  if (schemeSearch) return result.find(({ scheme }) => scheme === schemeSearch) as T extends string ? UriValues : UriValues[]
+  return result as T extends string ? UriValues : UriValues[]
+}
+
+export const toUri = ({ id, scheme }: { id: string, scheme: string }): Uri => `${scheme}:${id}`
+
+export const isUri = (uri: string): uri is Uri =>
   uri
     .split(':')
     .filter(part => part.length)
     .length === 2
 
-export const isUris = (uri: string) =>
+export const isUris = (uri: string): uri is Uris =>
   uri
     .split(',')
     .every(isUri)
