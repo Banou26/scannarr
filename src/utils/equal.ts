6fca8e7f0707276db7e02512500c4a2e8712c590
Banou <me@banou.dev>
2022-08-18 04:03:32 +0200

    feat: ✨define NameEq fp-ts Eq

--- src/utils/equal.ts
+++ src/utils/equal.ts
@@ -1,5 +1,11 @@
 import type { Genre } from '../types/genre'
+import Name from '../types/name'
 
 export const GenreEq = {
   equals: (genre: Genre, genre2: Genre) => genre.name === genre2.name
 }
+
+export const NameEq = {
+  equals: (name: Name, name2: Name) => name.name === name2.name
+}
+
