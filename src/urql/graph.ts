bfd229e245d17ede3c1a71399b6c5109356cfe9e
Banou <me@banou.dev>
2024-03-16 02:25:25 +0100

    refactor: ♻️continue impl graph database

--- src/urql/graph.ts
+++ src/urql/graph.ts
@@ -1,7 +1,6 @@
 import { Observable, Subject, combineLatest, firstValueFrom, map, mergeMap, of, scan, shareReplay, startWith, switchMap } from 'rxjs'
 
 import { merge } from '../utils'
-import { Episode, Media, MediaExternalLink, MediaTrailer, PlaybackSource, Team } from '../generated/graphql'
 
 export const recursiveRemoveNullable = (obj) =>
   Array.isArray(obj)
@@ -21,21 +20,16 @@ export const recursiveRemoveNullable = (obj) =>
     )
 
 type NodeData =
-  { __typename: string } &
+  { _id: string, __typename: string } &
   { [key: string]: any }
 
 type Node<NodeDataType extends NodeData> = {
-  id: string
+  _id: string
   $: Observable<NodeDataType>
-  set: (changes: Partial<NodeDataType>) => Promise<void>
+  subject: Subject<NodeDataType>
+  data: NodeDataType
+  update: (changes: Partial<NodeDataType>) => Promise<void>
   get: () => Promise<NodeDataType>
-  map: <T>(fn: (node: NodeDataType) => T) => Observable<UnwrapObservables<T>>
-}
-
-type KeyGenerator = (data: any) => string | null
-
-type KeyingConfig = {
-  [typename: string]: KeyGenerator
 }
 
 type ExtractObservableType<T> =
@@ -75,101 +69,142 @@ export const replaceObservablePairs = <T>(value: T, replacementPairs: [Observabl
   ) :
   value
 
-// todo: add custom merge function
-export const makeGraph = <NodeType extends NodeData>(
-  { keys }:
-  { keys: KeyingConfig }
-) => {
+const deepEqual = (a: any, b: any) => {
+  if (a === b) return true
+  if (typeof a !== 'object' || typeof b !== 'object') return false
+  if (Object.keys(a).length !== Object.keys(b).length) return false
+  for (const key in a) if (!deepEqual(a[key], b[key])) return false
+  return true
+}
+
+type Indexer<NodeType extends NodeData> = {
+  findOne: ({ filter, filteredNodes }: { filter: Partial<NodeType>, filteredNodes: Node<NodeType>[] }) => Node<NodeType> | undefined
+  insertOne: (node: Node<NodeType>) => void
+}
+
+const uidIndexer = <NodeType extends NodeData>(): Indexer<NodeType> => {
   const nodesMap = new Map<string, Node<NodeType>>()
-  console.log('nodesMap', nodesMap)
-  
-  const makeNode = <T extends NodeType>(_node: T) => {
-    const id = `${_node.__typename}:${keys[_node.__typename]?.(_node) as string}`
-
-    if (nodesMap.has(id)) {
-      throw new Error(`Node with id "${id}" already exists`)
-    }
-
-    const changesObservable = new Subject<T>()
-    const nodeObservable =
-      changesObservable
-        .pipe(
-          startWith(_node),
-          shareReplay(1)
-        )
-
-    if (!id) throw new Error(`No key for ${_node.__typename}`)
-
-    const node = {
-      __type__: 'Node',
-      id,
-      $: nodeObservable,
-      set: (changes: Partial<T>) =>
-        firstValueFrom(nodeObservable)
-          .then(node =>
-            changesObservable.next(
-              merge(node, changes) as T
-            )
-          ),
-      get: () => firstValueFrom(nodeObservable),
-      map: <T2 extends (node: T) => any>(fn: T2) =>
-        nodeObservable
-          .pipe(
-            switchMap(node => {
-              const result = fn(node)
-              const observables = getObservables(result)
-              if (!observables.length) return of(result)
-              const allObservablesResults = combineLatest(observables as Observable<any>)
-              return (
-                allObservablesResults
-                  .pipe(
-                    map(results =>
-                      replaceObservablePairs(
-                        result,
-                        results.map((result, i) => [
-                          (observables as Observable<any>[])[i]!,
-                          result
-                        ])
-                      )
-                    )
-                  )
-              )
-            })
-          )
-    } as Node<T>
-    nodesMap.set(id, node as Node<NodeType>)
-    return node
+  return {
+    findOne: ({ filter }) => filter._id ? nodesMap.get(filter._id) : undefined,
+    insertOne: (node) => nodesMap.set(node._id, node)
   }
+}
+
+const deepEqualIndexer = <NodeType extends NodeData>(): Indexer<NodeType> => {
+  let nodes: Node<NodeType>[] = []
+  return {
+    findOne: ({ filter, filteredNodes }) =>
+      filteredNodes
+        .find(_node => deepEqual(filter, _node.data)),
+    insertOne: (node) => {
+      nodes = [...nodes, node]
+    }
+  }
+}
+
+export const makeInMemoryGraphDatabase = <NodeType extends NodeData>(
+  { indexers: _indexers }:
+  { indexers?: Indexer<NodeType>[] }
+) => {
+  const indexers = [
+    uidIndexer<NodeType>(),
+    ..._indexers ?? [],
+    deepEqualIndexer<NodeType>()
+  ]
+  const nodes = new Map<string, Node<NodeType>>()
+
+  const findNodeOne = (filter: Partial<NodeType>) =>
+    indexers
+      .reduce(
+        (filteredNodes, indexer) => {
+          const indexResult = indexer.findOne({ filteredNodes, filter })
+          return (
+            indexResult
+              ? [indexResult]
+              : filteredNodes
+          )
+        },
+        [...nodes.values()]
+      )
+      .at(0)
 
   return {
-    makeNode,
-    hasNode: (id: string) => nodesMap.has(id),
-    getNode: (id: string) => nodesMap.get(id)
+    findOne: <T extends NodeType>(filter: Partial<T>) => findNodeOne(filter)?.data,
+    mapOne: <T2 extends (nodeData: NodeType, node: Node<NodeType>) => any>(filter: Partial<NodeType>, fn: T2) => {
+      const foundNode = findNodeOne(filter)
+      if (!foundNode) throw new Error(`No node found for ${JSON.stringify(filter)}`)
+      return (
+        foundNode
+        .$
+        .pipe(
+          switchMap(node => {
+            const result = fn(node, foundNode)
+            const observables = getObservables(result)
+            if (!observables.length) return of(result)
+            return (
+              combineLatest(observables as Observable<any>)
+                .pipe(
+                  map(results =>
+                    replaceObservablePairs(
+                      result,
+                      results.map((result, i) => [
+                        (observables as Observable<any>[])[i]!,
+                        result
+                      ])
+                    )
+                  )
+                )
+            )
+          })
+        )
+      )
+    },
+    insertOne: <T extends NodeType>(_node: T) => {
+      const _id = globalThis.crypto.randomUUID() as string
+      const data = { ..._node, _id }
+
+      const changesObservable = new Subject<T>()
+      const nodeObservable =
+        changesObservable
+          .pipe(
+            startWith(data),
+            shareReplay(1)
+          )
+  
+      if (!_id) throw new Error(`No key for ${_node.__typename}`)
+  
+      const node = {
+        __graph_type__: 'Node',
+        _id,
+        subject: changesObservable,
+        $: nodeObservable,
+        data,
+        update: (changes: Partial<T>) =>
+          firstValueFrom(nodeObservable)
+            .then(node =>
+              changesObservable.next(
+                merge(node, changes) as T
+              )
+            ),
+        get: () => firstValueFrom(nodeObservable)
+      } as Node<T>
+
+      nodes.set(_id, node as Node<T>)
+      for (const indexer of indexers) indexer.insertOne(node)
+
+      return data
+    },
+    updateOne: <T extends NodeType>(filter: Partial<T>, changes: Partial<T>) => {
+      const node = findNodeOne(filter)
+      if (!node) throw new Error(`No node found for ${JSON.stringify(filter)}`)
+      firstValueFrom(node.$)
+        .then(nodeData =>
+          node.subject.next(
+            merge(nodeData, changes) as T
+          )
+        )
+    }
   }
 }
 
-export const keyResolvers = {
-  UserMediaPage: () => null,
-  Authentication: () => null,
-  AuthenticationMethod: () => null,
-  MediaPage: () => null,
-  Media: (media) => (media as Media).uri,
-  MediaConnection: () => null,
-  MediaEdge: () => null,
-  Episode: (episode) => (episode as Episode).uri,
-  EpisodePage: () => null,
-  EpisodeConnection: () => null,
-  EpisodeEdge: () => null,
-  PlaybackSourcePage: () => null,
-  PlaybackSource: (playbackSource) => (playbackSource as PlaybackSource).uri,
-  Team: (team) => (team as Team).uri,
-  PlaybackSourceConnection: () => null,
-  PlaybackSourceEdge: () => null,
-  MediaExternalLink: (mediaExternalLink) => (mediaExternalLink as MediaExternalLink).uri,
-  MediaTrailer: (mediaTrailer) => (mediaTrailer as MediaTrailer).uri,
-  MediaCoverImage: () => null,
-  MediaTitle: () => null,
-  FuzzyDate: () => null,
-}
-
-export type Graph = ReturnType<typeof makeGraph>
+export type InMemoryGraphDatabase = ReturnType<typeof makeInMemoryGraphDatabase>
