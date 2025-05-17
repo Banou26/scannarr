417c7358e2d0d99def8d9cec3f1d20c8625d1b22
Banou <me@banou.dev>
2025-05-17 14:46:59 +0900

    refactor: ♻️stash current stuff, broke af

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,69 @@
-import './patch'
-import * as Prisma from 'prisma-client-generated'
-import { PrismaClient } from 'prisma-client-generated'
+// import './patch'
+// import * as Prisma from '../prisma/generated/edge'
+// import * as Prisma from '../prisma/generated/edge'
+// import { PrismaClient } from 'prisma-client-generated/edge'
+// import * as Prisma from 'prisma-client-generated/generated/client'
+// import * as Prisma from 'prisma-client-generated/edge'
+// import * as Prisma from '../prisma/generated/edge'
+// import { PrismaD1 } from '@prisma/adapter-d1'
+// import { PrismaD1HTTP } from '@prisma/adapter-d1'
 
-import { getQueryResults, runQuery } from './database'
-import { getAllMedia, insertMedia } from '../prisma/generated/sql'
-import { PrismaBetterSQLite3AdapterFactory } from './adapter'
+// const CLOUDFLARE_D1_TOKEN = import.meta.env.CLOUDFLARE_D1_TOKEN
+// const CLOUDFLARE_ACCOUNT_ID = import.meta.env.CLOUDFLARE_ACCOUNT_ID
+// const CLOUDFLARE_DATABASE_ID = import.meta.env.CLOUDFLARE_DATABASE_ID
 
-console.log('Prisma', Prisma)
+// const adapter = new PrismaD1HTTP({
+//   CLOUDFLARE_D1_TOKEN,
+//   CLOUDFLARE_ACCOUNT_ID,
+//   CLOUDFLARE_DATABASE_ID,
+// })
 
-const adapter = new PrismaBetterSQLite3AdapterFactory({ url: '' })
+// console.log('Prisma', Prisma)
 
-const prisma = new PrismaClient({ adapter })
+// const Prisma = await import('../prisma/generated/edge')
 
-const op = prisma.media.create({
+
+import * as Prisma from 'prisma-client-generated/generated/edge'
+const { PrismaClient } = Prisma
+// import { PrismaClient } from 'prisma-client-generated/generated/client'
+
+const prisma = new PrismaClient({ datasourceUrl: 'file:./dev.db' })
+
+const op = await prisma.media.create({
   data: {
     id: '1',
     name: 'test',
   }
 })
+
 console.log('op', op)
 
-// console.log('prisma', prisma)
+// import './patch'
+// import * as Prisma from 'prisma-client-generated'
+// import { PrismaClient } from 'prisma-client-generated'
 
-// console.log('exec',
-//   await runQuery(insertMedia, ['1', 'test'])
-// )
+// import { getQueryResults, runQuery } from './database'
+// import { getAllMedia, insertMedia } from '../prisma/generated/sql'
+// import { PrismaBetterSQLite3AdapterFactory } from './adapter'
 
-// console.log('assetLatestPools', await getQueryResults(getAllMedia, ['test']))
+// console.log('Prisma', Prisma)
+
+// const adapter = new PrismaBetterSQLite3AdapterFactory({ url: '' })
+
+// const prisma = new PrismaClient({ adapter })
+
+// const op = prisma.media.create({
+//   data: {
+//     id: '1',
+//     name: 'test',
+//   }
+// })
+// console.log('op', op)
+
+// // console.log('prisma', prisma)
+
+// // console.log('exec',
+// //   await runQuery(insertMedia, ['1', 'test'])
+// // )
+
+// // console.log('assetLatestPools', await getQueryResults(getAllMedia, ['test']))
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
+
+import polyfills from './vite-plugin-node-stdlib-browser.cjs'
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+    outDir: 'build',
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ]
+    }
+  },
+  plugins: [
+    polyfills()
+  ],
   optimizeDeps: {
     include: [
       'prisma-client-generated',
+      'prisma-client-generated/generated/client',
+      'prisma-client-generated/generated/edge'
     ],
     exclude: [
       'wa-sqlite',
